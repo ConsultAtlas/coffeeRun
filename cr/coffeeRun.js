1a07d3eba9e19a5dcc04947fbb2f716a80c6ff01
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-21 19:50:03 -0400

    added some id tags to radio iputs and created jquery variables

---
+++ cr/coffeeRun.js
@@ -0,0 +1,19 @@
+
+
+$(function (){
+
+  // Here we need to save the
+  $('#data-coffee-order').submit(function(){
+    // get values for submission fields
+    var coffeeOrder = $('#coffeeOrder').val();
+    var email = $('#emailInput').val();
+    var small = $('#short').val();
+    var medium = $('#tall').val();
+    var large = $('#grande').val();
+    var flavor = $('#flavorShot').val();
+    var caffine = $('#strengthLevel').val();
+  })
+
+
+
+})
