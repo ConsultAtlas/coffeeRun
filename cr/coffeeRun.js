0af053eb4aff7e267f9234e499f34a7f420d1ea9
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-21 21:01:01 -0400

    added js for the basic assignment

--- cr/coffeeRun.js
+++ cr/coffeeRun.js
@@ -2,18 +2,48 @@
 
 $(function (){
 
+  // here we need to pupulate the forms fields with the values that were saved to localStorage
+  var coffeeOrder = localStorage.getItem('coffeeOrder', coffeeOrder);
+  var email = localStorage.getItem('email', email);
+  var size = localStorage.getItem('size', size);
+  var flavor = localStorage.getItem('flavor', flavor);
+  var caffeine = localStorage.getItem('caffeine', caffeine);
+
+  $('#coffeeOrder').val(coffeeOrder);
+  $('#emailInput').val(email);
+
+  if (size) {
+    $("input[name=size]").attr("checked", false);
+    var elementToSelect= "input[value=" + size + "]";
+    $(elementToSelect).attr("checked", true);
+    }
+
+  $('input:checked').val();
+  $('#flavorShot').val(flavor);
+  $('#strengthLevel').val(caffeine);
+
   // Here we need to save the
-  $('#data-coffee-order').submit(function(){
+  $('form').submit(function(event){
+    // prevent the forms default behavior of refreshing upon submit
+    event.preventDefault();
+
     // get values for submission fields
     var coffeeOrder = $('#coffeeOrder').val();
     var email = $('#emailInput').val();
-    var small = $('#short').val();
-    var medium = $('#tall').val();
-    var large = $('#grande').val();
+    var size = $('input:checked').val();
     var flavor = $('#flavorShot').val();
-    var caffine = $('#strengthLevel').val();
-  })
+    var caffeine = $('#strengthLevel').val();
 
 
 
-})
+    // Set values submitted by user to local form
+    localStorage.setItem('coffeeOrder', coffeeOrder);
+    localStorage.setItem('email', email);
+    localStorage.setItem('size', size);
+    localStorage.setItem('flavor', flavor);
+    localStorage.setItem('caffeine', caffeine);
+  });
+
+
+
+});
